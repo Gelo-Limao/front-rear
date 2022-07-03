3d0b97fc2e0bdc8825288c49de0bcefa83740ac2
euclides.silva.acc <euclides.silva@accurate.com.br>
2022-07-03 03:16:34 -0300

    fix authAPI

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const authAPI = axios.create({
+const authAPI = axios.create({
     baseURL: 'https://api-test-geloelimao.herokuapp.com',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
 });
+
+export default authAPI
